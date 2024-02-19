7df9bca14fc6999b72e3b5fe0a3f8a5f127e6af9
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-19 19:42:22 -0300

    Feat: Include condicional options to navegate throgth menu

--- app.py
+++ app.py
@@ -15,3 +15,12 @@ print('4. Sair')
 
 opcao_escolhida = input('Escolha uma opção: ')
 print(f'Você escolhe a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante')
+else:
+    print('Encerrando o programa')
