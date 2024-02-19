9301e1e4181ab8d5c92c6257e3358b2c24784d04
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-19 19:30:27 -0300

    Feat: include an input to choose on the menu options

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ print('1. Cadastrar Restaurante')
 print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
 print('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolhe a opção ', opcao_escolhida)
