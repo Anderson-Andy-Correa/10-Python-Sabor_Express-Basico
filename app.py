8edb34861334ef46a07e29e98f908b52e01b2393
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-19 19:59:00 -0300

    Feat: add methods exibir_nome_do_programa, exibir_opcoes, escolher_opcao and main

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-
-print('''
+def exibir_nome_do_programa():
+    print('''
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -10,22 +10,36 @@ print('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 ''') # Fsymbols.com
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair')
+
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
 
 def finalizar_app():
     os.system('cls') # No Windows
     print('Finalizando o app\n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar Restaurante')
-elif opcao_escolhida == 2:
-    print('Listar Restaurante')
-elif opcao_escolhida == 3:
-    print('Ativar Restaurante')
-else:
-    finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
